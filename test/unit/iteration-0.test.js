e8737297b6909a56ec1fab193f077476a0d9e5ca
Christie Lynam <christielynam@gmail.com>
2018-03-06 12:54:31 -0700

    Update iteration-0.test.js

--- test/unit/iteration-0.test.js
+++ test/unit/iteration-0.test.js
@@ -6,13 +6,13 @@ describe('DistrictRepository iteration 0', () =>  {
 
   test('district has data in an object', () => {
     // remember that an array is also just an object.
-    expect(typeof district.data).toBe('object');
+    expect(typeof district.stats).toBe('object');
   });
 
   test('data coming in has no duplicates', () => {
     // uncomment out the tests that best fits your model
-    // expect(district.data.length).toBe(181);
-    expect(Object.keys(district.data).length).toBe(181);
+    // expect(district.stats.length).toBe(181);
+    expect(Object.keys(district.stats).length).toBe(181);
   });
 
 });
