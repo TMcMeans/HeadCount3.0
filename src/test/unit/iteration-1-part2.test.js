8656ba592c2b254b92bd14371bc5cfbfc2b492b3
Will Mitchell <wvmitchell@gmail.com>
2018-05-10 10:17:51 -0600

    move tests into src

--- src/test/unit/iteration-1-part2.test.js
+++ src/test/unit/iteration-1-part2.test.js
@@ -1,4 +1,4 @@
-import DistrictRepository from '../../src/helper.js';
+import DistrictRepository from '../../helper.js';
 import kinderData from '../../data/kindergartners_in_full_day_program.js';
 
 describe('DistrictRepository iteration 1 - part 2', () =>  {
