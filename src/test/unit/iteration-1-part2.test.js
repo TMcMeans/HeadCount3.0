687d7fcefb11f82986c9932f15231469b5618795
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-11-27 14:12:51 -0700

    Add findAllMatches method functionality in helper.js

--- src/test/unit/iteration-1-part2.test.js
+++ src/test/unit/iteration-1-part2.test.js
@@ -8,11 +8,11 @@ describe('DistrictRepository iteration 1 - part 2', () => {
     expect(district.findAllMatches().length).toBe(181);
   });
 
-  test.skip('findAllMatches returns matches in an array, case insensitive', () => {
+  test('findAllMatches returns matches in an array, case insensitive', () => {
     expect(district.findAllMatches('ColoRado').length).toBe(2);
   });
 
-  test.skip('findAllMatches finds no matches and returns an empty array when given arguments that dont exist within the data', () => {
+  test('findAllMatches finds no matches and returns an empty array when given arguments that dont exist within the data', () => {
     expect(district.findAllMatches('al;dkfjas;dlkjasdfl;').length).toBe(0);
     expect(district.findAllMatches('packers').length).toBe(0);
     expect(district.findAllMatches('df').length).toBe(0);
