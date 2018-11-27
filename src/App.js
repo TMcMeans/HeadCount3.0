6cd59df347fe46c6fa1940b46979ff7d331376c8
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-11-27 15:01:40 -0700

    Add cardcontainer component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import kinderData from './data/kindergartners_in_full_day_program.js';
+
 import './App.css';
+import DistrictRepository from './helper.js';
+import { CardContainer } from './components/CardContainer';
+
+const repository = new DistrictRepository(kinderData);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      respository: new DistrictRepository(kinderData)
+    };
+  }
+
+  componentDidMount = () => {};
+
   render() {
+    const { stats } = this.state.respository;
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <h1>Welcome to HeadCount 3.0</h1>
+        <CardContainer />
+      </div>
     );
   }
 }
