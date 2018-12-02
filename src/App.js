0b25547801db249326abf39b63ccbdc4925357c4
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-12-01 20:05:18 -0700

    Add prop to CardContainer

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ class App extends Component {
           />
           <CardContainer
             findAllMatches={() => findAllMatches(inputSearchName)}
+            handleCompareSelections={this.handleCompareSelections}
           />
         </div>
       );
