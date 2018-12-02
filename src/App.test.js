662bb9fa33c55a336e58b40127568924defddbe8
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-12-02 13:50:24 -0700

    Add unit tests to App

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 
-import { shallow } from 'enzyme';
+import kinderData from './data/kindergartners_in_full_day_program.js';
+import DistrictRepository from './helper.js';
 
 describe('App', () => {
   it('should match snapshot with all html elements rendered correctly', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state with district repository data and properties when App is mounted', () => {
+    const wrapper = shallow(<App />);
+
+    const expectedState = {
+      repository: new DistrictRepository(kinderData),
+      inputSearchName: '',
+      compareSchoolSelections: []
+    };
+
+    expect(wrapper.state()).toEqual(expectedState);
+  });
+
+  it('should update state with inputSearchName when filterCards is called', () => {
+    const mockInputName = 'colorado';
+    const wrapper = mount(<App />);
+
+    wrapper.instance().filterCards(mockInputName);
+    expect(wrapper.state('inputSearchName')).toEqual(mockInputName);
+  });
+
+  it('should update state to add school names to compareSchoolSelections array when handleCompareSelections is called', () => {
+    const wrapper = mount(<App />);
+    const school1 = 'COLORADO';
+    const school2 = 'ADAMS COUNTY 14';
+
+    const mockCompareSelections = ['ADAMS COUNTY 14', 'COLORADO'];
+
+    wrapper.instance().handleCompareSelections(school1);
+    wrapper.instance().handleCompareSelections(school2);
+
+    expect(wrapper.state('compareSchoolSelections')).toEqual([
+      'ADAMS COUNTY 14',
+      'COLORADO'
+    ]);
+  });
+
+  it('should remove an added schoolName from state once removeCompraredSelections is called', () => {
+    const wrapper = mount(<App />);
+    const school1 = 'COLORADO';
+    const school2 = 'ADAMS COUNTY 14';
+
+    const mockCompareSelections = ['ADAMS COUNTY 14', 'COLORADO'];
+
+    wrapper.instance().handleCompareSelections(school1);
+    wrapper.instance().handleCompareSelections(school2);
+
+    wrapper.instance().removeCompareSelections('ADAMS COUNTY 14');
+
+    expect(wrapper.state('compareSchoolSelections')).toEqual(['COLORADO']);
   });
 });
