82a7c6aeb05e574442a877527cf90d18d0b128ed
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-12-01 19:58:16 -0700

    Add conditional rendering logic to CompareCard

--- src/components/CompareCardContainer.js
+++ src/components/CompareCardContainer.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import { Card } from './Card';
 import './CompareCardContainer.css';
 import './CompareCard.css';
 
@@ -28,24 +26,52 @@ export const CompareCardContainer = ({
   } else if (compareSchoolSelections.length === 2) {
     const schoolData1 = findAllMatches(compareSchoolSelections[0]);
     const schoolData2 = findAllMatches(compareSchoolSelections[1]);
-    return (
-      <div className="compare-container">
-        <CompareCard
-          className="compare-card"
-          location={schoolData1[0].location}
-          stats={schoolData1[0].stats}
-          key={Math.random()}
-          removeCompareSelections={removeCompareSelections}
-        />
-        <CompareCard
-          className="compare-card"
-          location={schoolData2[0].location}
-          stats={schoolData2[0].stats}
-          key={Math.random()}
-          removeCompareSelections={removeCompareSelections}
-        />
-      </div>
+    const comparedData = compareDistrictAverages(
+      compareSchoolSelections[0],
+      compareSchoolSelections[1]
     );
+    const comparedDataEntries = Object.entries(comparedData);
+    if (comparedDataEntries.length > 2) {
+      return (
+        <div className="compare-container">
+          <CompareCard
+            className="compare-card"
+            location={schoolData1[0].location}
+            stats={schoolData1[0].stats}
+            key={Math.random()}
+            removeCompareSelections={removeCompareSelections}
+          />
+          <div className="comparison-section">
+            <h1>COMPARE AVERAGES</h1>
+            <h1>
+              {comparedDataEntries[0][0]}: {comparedDataEntries[0][1]}
+            </h1>
+            <h1>
+              COMPARED: <br />
+              ---- {comparedDataEntries[2][1]} ----
+            </h1>
+            <h1>
+              {comparedDataEntries[1][0]}: {comparedDataEntries[1][1]}
+            </h1>
+          </div>
+          <CompareCard
+            className="compare-card"
+            location={schoolData2[0].location}
+            stats={schoolData2[0].stats}
+            key={Math.random()}
+            removeCompareSelections={removeCompareSelections}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="compare-container">
+          <h1>
+            You must select two DIFFERENT school cards to compare averages.
+          </h1>
+        </div>
+      );
+    }
   } else {
     return <div className="compare-container" />;
   }
